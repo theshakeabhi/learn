d0559db7f81a2de39c1d5ab9643f1bee9d106538
acnair <senan.jr97@gmail.com>
2020-03-28 22:33:51 +0530

    complted Chapter2 excersie

--- javaScript/B1-EloquentJS/trial/C2/e1.js
+++ javaScript/B1-EloquentJS/trial/C2/e1.js
@@ -1,3 +1,4 @@
+//Q1: non optimised.
 console.log("1. Triangle \n")
 for ( let outterLoop=1; outterLoop<=7; outterLoop++){
     for(let innerLoop=0; innerLoop<outterLoop; innerLoop++){
@@ -6,6 +7,18 @@ for ( let outterLoop=1; outterLoop<=7; outterLoop++){
     console.log('\n')
 }
 
+//Q1: optimised.
+console.log("1. Optimised sol.")
+let hash='';
+for(let loop=0; loop<7; loop++){
+    hash = hash + '#';
+    console.log(hash+'\n')
+}
+
+console.log('\n');
+console.log('\n');
+
+//Q2.
 // Important question look for better optimised version
 console.log("2. Buzz-fizz");
 for( let num=1; num<=100; num++){
@@ -24,6 +37,29 @@ for( let num=1; num<=100; num++){
    
 }
 
+console.log('\n');
+
+//Q2. Optimsed
+console.log("2. Buzz-fizz: Optimised");
+for( let optimisedNum=1; optimisedNum<=100; optimisedNum++){
+    if(optimisedNum%15===0){
+        console.log("Fizz-Buzz");
+    } 
+    else if(optimisedNum%3===0){
+        console.log("Fizz\n");
+    }
+    else if(optimisedNum%5===0){
+        console.log("Buzz\n");
+    }
+    else{
+        console.log(optimisedNum);
+    }
+    
+}
+
+console.log('\n');
+
+//Q3. 
 console.log("\n3. Chessboard");
 let flag=0;
 for(let oL=0; oL<8; oL++){
@@ -36,3 +72,21 @@ for(let oL=0; oL<8; oL++){
         }
     }
 }
+
+console.log('\n');
+
+//Q3. Optimised.
+console.log("3. Chessboard");
+let size=8;
+for (let i=0; i<size; i++){
+    for(let j=0; j<size; j++){
+        if((i+j)%2 == 0){
+            console.log("#");
+        }
+        else{
+            console.log(" ")
+        }
+    }
+}
+
+//something wrong with the code.
